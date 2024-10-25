474794f9efe6065847732843b1556642294ae2d0
qoqbobb <kail853@naver.com>
2024-10-25 09:52:05 +0900

    first commit

---
+++ src/main/java/com/example/toyboard/ToyboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.toyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyboardApplication.class, args);
+	}
+
+}
